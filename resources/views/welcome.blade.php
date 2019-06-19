cb433af49c166923376f33e4f023fd30cb5442e7
unknown <chingoski.nikola@gmail.com>
2019-06-19 15:01:12 +0200

    Laravel Ep5 / Sending data to your views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,15 @@
 
 @section('content')
     <h1 class="display-3 text-center">Laravel home page</h1>
-    <?php foreach ($tasks as $task):?>
-    <ul class="list-group">
-        <li class="list-group-item"><?= $task ?></li>
-    </ul>
-    <?php endforeach; ?>
+
+    <div class="row">
+        <div class="col col-sm-4">
+            <ul class="list-group">
+                @foreach($tasks as $task)
+                <li class="list-group-item">{{$task}}</li>
+                    @endforeach
+            </ul>
+        </div>
+    </div>
+
 @endsection
